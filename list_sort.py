0387d665e795e8b61a598a881c971576045b2d51
Jake Romer <jkrmr.io@gmail.com>
2017-05-28 03:23:42 -0400

    Add doc strings

--- list_sort.py
+++ list_sort.py
@@ -3,16 +3,29 @@ from node import Node
 
 class ListSort:
     def __init__(self, original_string):
-        self._node_list = [Node(x) for x in original_string.split()]
-        self._tag_list = [x.is_int() for x in self._node_list]
-        self._word_list = sorted([x for x in self._node_list if x.is_word()])
-        self._int_list = sorted([x for x in self._node_list if x.is_int()])
+        # tokenize the input string
+        node_list = [Node(x) for x in original_string.split()]
+        # create an associated list marking int / word positions
+        self._tag_list = [x.is_int() for x in node_list]
+        # filter and sort word and int nodes
+        self._word_list = sorted([x for x in node_list if x.is_word()])
+        self._int_list = sorted([x for x in node_list if x.is_int()])
 
     def zip_sorted(self):
+        """
+        "Zip" together sorted int and word values back into a list in the
+        appropriate positions.
+        """
         return ' '.join([self._int_or_word(x) for x in self._tag_list])
 
     def _int_or_word(self, is_int_position):
+        """
+        Pop a node off the front of the appropriate list and return its
+        tokenized value string.
+        """
         if is_int_position:
-            return str(self._int_list.pop(0).value())
+            appropriate_list = self._int_list
         else:
-            return self._word_list.pop(0).value()
+            appropriate_list = self._word_list
+
+        return appropriate_list.pop(0).value_string()
--- node.py
+++ node.py
@@ -2,24 +2,46 @@ import re
 
 
 class Node:
-    def __init__(self, str):
-        self._str = re.sub(r'[^0-9A-Za-z]', '', str)
-        self._is_int = not not re.match(r'[0-9]', str)
+    """
+    Wraps a string token, decides whether it's a word or an int,
+    returns appropriate value via value().
+
+    Implements __lt__ to be orderable.
+    """
+
+    def __init__(self, token):
+        self._token = re.sub(r'[^0-9A-Za-z]', '', token)
+        self._is_int = not not re.match(r'[0-9]', token)
 
     def is_int(self):
+        """
+        Returns: True if an int node. False otherwise.
+        """
         return self._is_int
 
     def is_word(self):
+        """
+        Returns: True if a word node. False otherwise.
+        """
         return not self.is_int()
 
+    def value_string(self):
+        """
+        Returns: The tokenized input string.
+        """
+        return self._token
+
     def value(self):
+        """
+        Parses a stringified integer to an Int if an int node.
+        Otherwise, just returns the string.
+
+        Returns: A String if a word node, an Int if an int node.
+        """
         if self.is_int():
-            return int(self._str)
+            return int(self.value_string())
         else:
-            return self._str
+            return self.value_string()
 
     def __lt__(self, other):
         return self.value().__lt__(other.value())
-
-    def __eq__(self, other):
-        return self.value().__eq__(other.value())
