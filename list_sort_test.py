57d21012ff2ab5d6d162e9579b1c44e00b1f011f
Jake Romer <jkrmr.io@gmail.com>
2017-05-28 04:02:48 -0400

    Add test cases for uppercase, negative numbers

--- list_sort_test.py
+++ list_sort_test.py
@@ -4,9 +4,29 @@ from list_sort import ListSort
 
 
 class TestListSort(TestCase):
-    def test_sorts_correctly(self):
+    def test_handles_empty_input(self):
+        result = ListSort('').zip_sorted()
+        self.assertEqual('', result)
+
+    def test_sorts_simple_cases_correctly(self):
         input = 'ba!!ker 55 c 5 a f d 1%0 h e 1'
         expected_output = 'a 1 baker 5 c d e 10 f h 55'
+
+        result = ListSort(input).zip_sorted()
+
+        self.assertEqual(expected_output, result)
+
+    def test_sorts_uppercased_strings_correctly(self):
+        input = 'Ba!!ker 55 c 5 a f d 1%0 h e 1'
+        expected_output = 'Baker 1 a 5 c d e 10 f h 55'
+
+        result = ListSort(input).zip_sorted()
+
+        self.assertEqual(expected_output, result)
+
+    def test_sorts_negative_numbers_correctly(self):
+        input = 'ba!!ke-r 5-5 c 5 a f d -1%0 h e -1'
+        expected_output = 'a -10 baker -1 c d e 5 f h 55'
 
         result = ListSort(input).zip_sorted()
 
