7f5ad476badf995df54f4e5a15c8304fb3f4e293
Jake Romer <jkrmr.io@gmail.com>
2017-05-29 09:57:27 -0400

    Accept input from stdin

--- zipsort.py
+++ zipsort.py
@@ -8,14 +8,16 @@ if __name__ == '__main__':
 
     Example:
         python zipsort.py data/sample.txt
-    """
 
+    Alternatively, stream input from stdin:
+
+    cat INPUT_FILE_PATH | python zipsort.py
+    """
     try:
         path_to_input = sys.argv[1]
         input_list = open(path_to_input).read()
     except IndexError:
-        print(usage)
-        exit(1)
+        input_list = sys.stdin.read()
     except FileNotFoundError:
         print('Invalid input path: %s' % path_to_input)
         print(usage)
